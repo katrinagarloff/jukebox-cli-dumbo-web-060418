c9c53d0ec058f5c7e50db03b9d05fb656b218245
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-28 18:16:43 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -14,6 +14,6 @@ def say_hello(name)
 "Hi #{name}"
 end
 
-puts "Enter your name:"
-users_name = gets.chomp 
-puts say_hello(users_name)
+#puts "Enter your name:"
+#users_name = gets.chomp 
+#puts say_hello(users_name)
