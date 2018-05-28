3b566516cb72f9f8ea9c47cd8b70edc67c4f1f82
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-28 18:18:18 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -15,7 +15,11 @@ def say_hello(name)
 end
 
 def help 
-  
+ puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
 end
 #puts "Enter your name:"
 #users_name = gets.chomp 
